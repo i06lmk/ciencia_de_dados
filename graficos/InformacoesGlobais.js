b9f2e6a39551255b5cf1f1fea65f19a7e1a00d95
Isadora Irmer <113686599+i06lmk@users.noreply.github.com>
2024-11-06 19:13:16 -0300

    save

--- graficos/InformacoesGlobais.js
+++ graficos/InformacoesGlobais.js
@@ -14,8 +14,6 @@ async function vizualizarInformacoesGlobais() {
     paragrafo.classList.add('graficos-container_texto')
     paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> bilhões de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
     console.log(paragrafo)
-    
-  // código omitido
 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
