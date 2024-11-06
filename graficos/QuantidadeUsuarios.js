4f8285d204030588ad1a3af8371f7cf72829a55c
Isadora Irmer <113686599+i06lmk@users.noreply.github.com>
2024-11-06 18:33:58 -0300

    save

--- graficos/QuantidadeUsuarios.js
+++ graficos/QuantidadeUsuarios.js
@@ -1,3 +1,5 @@
+import { getCSS } from "./common.js";
+
 async function quantidadeUsuarios() {
     const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero_usuarios.json'
     const res = await fetch(url)
@@ -10,15 +12,20 @@ async function quantidadeUsuarios() {
           y: quantidadeUsuarios,
           type: 'bar',
           marker: {
-            color: getComputedStyle(document.body)
+            color: getCSS('--primary-color')
           }
         }
       ]
+
+      const layout = {
+        plot_bgcolor: getCSS('--bg-color')
+        paper_bgcolor: getCSS('--bg-color')
+    }
       
     const grafico = document.createElement('div')
     grafico.className = 'grafico'
     document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newPlot(grafico, data)
+    Plotly.newPlot(grafico, data, layout)
       
       console.log(dados)
   }
